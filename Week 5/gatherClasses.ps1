327c7fd080b5bd399da2159ca46e61a0175c240a
derekrj28 <derekrj28@gmail.com>
2024-09-29 15:40:49 -0400

    week 5

---
+++ Week 5/gatherClasses.ps1
@@ -0,0 +1,62 @@
+﻿function DaysTranslator($Days) {
+    $DaysArray = @()
+
+    if ($Days -ilike "*M*") {
+        $DaysArray += "Monday"
+    }
+    if ($Days -ilike "*T*") {
+        $DaysArray += "Tuesday"
+    }
+    if ($Days -ilike "*W*") {
+        $DaysArray += "Wednesday"
+    }
+    if ($Days -ilike "*Th*") {
+        $DaysArray += "Thursday"
+    }
+    if ($Days -ilike "*F*") {
+        $DaysArray += "Friday"
+    }
+    if ($Days -ilike "*Sa*") {
+        $DaysArray += "Saturday"
+    }
+    if ($Days -ilike "*Su*") {
+        $DaysArray += "Sunday"
+    }
+
+    return $DaysArray
+}
+
+function gatherClasses(){
+
+    $page = Invoke-WebRequest -TimeoutSec 2 -Uri "http://localhost/Courses-1.html"
+    
+    # Get all the tr elements of HTML Document
+    $trs = $page.ParsedHtml.getElementsByTagName("tr")
+
+    # Array to hold results
+    $FullTable = @()
+    for($i = 1; $i -le $trs.Length; $i++){
+
+        # Get every td element of current tr element
+        $tds = $trs[$i-1].getElementsByTagName("td")
+
+        if ($tds.Length -ge 10) {
+            # want to seperate start time and end time from one time field
+            $Times = $tds[5].InnerText.Split(" - ")
+
+            $Days = DaysTranslator $tds[4].InnerText
+
+            $FullTable += [PSCustomObject]@{
+                "Class Code" = $tds[0].InnerText
+                "Title" = $tds[1].InnerText
+                "Days" = $Days
+                "Time Start" = $Times[0]
+                "Time End" = $Times[1]
+                "Instructor" = $tds[6].InnerText
+                "Location" = $tds[9].InnerText
+            }
+        }
+    }
+
+    $FullTable
+}
---
+++ Week 5/main.ps1
@@ -0,0 +1,19 @@
+﻿# Import the functions from the first script file
+. (Join-Path $PSScriptRoot 'gatherClasses.ps1')
+
+$results = gatherClasses
+
+# List all the classes of Instructor Furkan Paligu
+# $results | Where-Object {$_.Instructor -like "Furkan Paligu"} | Select-Object "Class Code", Instructor, Location, Days, "Time Start", "Time End"
+
+$ITSInstructors = $results | Where-Object {($_."Class Code" -ilike "SYS*") -or ($_."Class Code" -ilike "NET*") -or ($_."Class Code" -ilike "SEC*") -or ($_."Class Code" -ilike "FOR*") -or ($_."Class Code" -ilike "CSI*") -or ($_."Class Code" -ilike "DAT*")}
+
+# output just the ITS instructors
+$ITSInstructors | Format-Table -Property Instructor
+
+# List ITS instructors and list in decending order by class count
+$ITSInstructors | Group-Object -Property Instructor | 
+    Select-Object @{Name='Instructor';Expression={$_.Name}}, 
+                  @{Name='Count';Expression={$_.Count}} | 
+    Sort-Object -Property Count -Descending | 
+    Format-Table -AutoSize
