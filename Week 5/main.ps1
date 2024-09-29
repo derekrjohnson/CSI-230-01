a9eb68b08cfd9275a98e1e9cccb707860a0f219a
derekrj28 <derekrj28@gmail.com>
2024-09-29 15:54:29 -0400

    week 5

--- Week 5/main.ps1
+++ Week 5/main.ps1
@@ -4,8 +4,12 @@
 $results = gatherClasses
 
 # List all the classes of Instructor Furkan Paligu
-# $results | Where-Object {$_.Instructor -like "Furkan Paligu"} | Select-Object "Class Code", Instructor, Location, Days, "Time Start", "Time End"
+$results | Where-Object {$_.Instructor -like "Furkan Paligu"} | Select-Object "Class Code", Instructor, Location, Days, "Time Start", "Time End"
 
+# List all the classes of JOYC 310 Mondays
+$results | Where-Object { ($_.Location -like "JOYC 310") -and ($_.days -contains "Monday") } | Sort-Object "Time Start" | Format-Table "Time Start", "Time End", "Class Code"
+
+# Get ITS instructors
 $ITSInstructors = $results | Where-Object {($_."Class Code" -ilike "SYS*") -or ($_."Class Code" -ilike "NET*") -or ($_."Class Code" -ilike "SEC*") -or ($_."Class Code" -ilike "FOR*") -or ($_."Class Code" -ilike "CSI*") -or ($_."Class Code" -ilike "DAT*")}
 
 # output just the ITS instructors
