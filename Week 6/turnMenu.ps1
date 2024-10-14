2921e0547e43eae9eedc9dc4b9161d20b9d71c24
derekrj28 <derekrj28@gmail.com>
2024-10-14 13:34:47 -0400

    week 6

--- Week 6/turnMenu.ps1
+++ Week 6/turnMenu.ps1
@@ -1,84 +1,60 @@
-﻿function Get-LogInOuts {
-    param ($lookback)
+﻿& (Join-Path (Join-Path $parentDir "Week 4") "Apache-Logs.ps1")
+& (Join-Path $PSScriptRoot "Event-Logs.ps1")
+& (Join-Path (Join-Path $parentDir "Week 2") "Process-Management-4.ps1")
 
-    $logInOuts = Get-EventLog -LogName System -InstanceID 7001, 7002 -After (Get-Date).AddDays($lookback)
+$Prompt = "`n"
+$Prompt += "Please choose your operation:`n"
+$Prompt += "1 - Display the last 10 apache logs`n"
+$Prompt += "2 - Last 10 failed logons for all users`n"
+$Prompt += "3 - Display at risk users`n"
+$Prompt += "4 - Start chrome and go to champlain`n"
+$Prompt += "5 - Exit`n"
 
-    $logInOutsTable = @()
-    for ($i=0; $i -lt $logInOuts.Count; $i++) {
+$operation = $true
 
-        # Creating event property value
-        $event = ""
-        if ($logInOuts[$i].InstanceID -eq 7001) {$event="Logon"}
-        if ($logInOuts[$i].InstanceID -eq 7002) {$event="Logoff"}
+while($operation){
 
-        # Creating event property value
-        $SID = New-Object System.Security.Principal.SecurityIdentifier($logInOuts[$i].ReplacementStrings[1])
-        $user = $SID.Translate([System.Security.Principal.NTAccount]).Value
+    
+    Write-Host $Prompt | Out-String
+    $choice = Read-Host 
 
-        # Adding each new line to our empty array
-        $logInOutsTable += [PSCustomObject]@{
-            "Time" = $logInOuts[$i].TimeGenerated.ToString("MM/dd/yyyy hh:mm:ss tt");
-            "Id" = $logInOuts[$i].InstanceID;
-            "Event" = $event;
-            "User" = $user;
-        }
+
+    if($choice -eq 5){
+        Write-Host "Goodbye" | Out-String
+        exit
+        $operation = $false 
     }
 
-    $logInOutsTable
-}
+    elseif($choice -eq 1){
+        GetApacheLogs "index.html" 200 "Chrome" | Select-Object -Last 10 | Format-Table
+    }
 
-# Call the function with the parameter and print the results on the screen
-$logInOutsResult = Get-LogInOuts -lookback -14
-$logInOutsResult | Format-Table
+    elseif($choice -eq 2){
+        getFailedLogins 10 | Select-Object -Last 10 | Format-Table
+    }
 
+    elseif($choice -eq 3){
+        $failedLogons = getFailedLogins 10
+        Write-Host ($failedLogins | Group User | Where-Object { $_.Count -gt 10 } | Select-Object Count, Name  | Format-Table | Out-String)
+    }
+    elseif($choice -eq 4){
+        # Set chrome variables
+        $pathToChrome = 'C:\Program Files\Google\Chrome\Application\chrome.exe'
+        $tempFolder = '--user-data-dir=c:\temp' # pick a temp folder for user data
+        $startmode = '--start-fullscreen' # '--kiosk' is another option
+        $startPage = 'https://champlain.edu'
 
+        # get the running chrome processes
+        $chromeRunningProcesses = Get-Process | Where-Object { $_.ProcessName -like "chrome" } 
 
-
-
-
-# Initiate the function
-function GetApacheLogs {
-
-    # Set parameters
-    param (
-        [string]$pageVisited,
-        [int]$responseCode,
-        [string]$webBrowser
-    )
-
-    # define regex pattern
-    $regex = [regex] "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}."
-
-    # Get the logs that match
-    $logEntries = Get-Content C:\xampp\apache\logs\access.log | Where-Object { $_ -like "*$pageVisited*" -and $_ -like "*$responseCode*" -and $_ -like "*$webBrowser*" }
-    
-    # Extract the ip address from the logs
-    $ipAddresses = $regex.Matches($logEntries)
-    return $ipAddresses
-
-}
-
-# Call the function
-GetApacheLogs "index.html" 200 "Chrome"
-
-
-
-
-# At risk users
-
-
-# Set chrome variables
-$pathToChrome = 'C:\Program Files\Google\Chrome\Application\chrome.exe'
-$tempFolder = '--user-data-dir=c:\temp' # pick a temp folder for user data
-$startmode = '--start-fullscreen' # '--kiosk' is another option
-$startPage = 'https://champlain.edu'
-
-# get the running chrome processes
-$chromeRunningProcesses = Get-Process | Where-Object { $_.ProcessName -like "chrome" } 
-
-if ($chromeRunningProcesses -eq $null -or $chromeRunningProcesses.Count -eq 0) {
-    Start-Process -FilePath $pathToChrome -ArgumentList $tempFolder, $startmode, $startPage
-} 
-else {
-    Get-Process | Where-Object { $_.ProcessName -like "chrome" } | Stop-Process -Force
-}
+        if ($chromeRunningProcesses -eq $null -or $chromeRunningProcesses.Count -eq 0) {
+            Start-Process -FilePath $pathToChrome -ArgumentList $tempFolder, $startmode, $startPage
+        } 
+        else {
+            Get-Process | Where-Object { $_.ProcessName -like "chrome" } | Stop-Process -Force
+        }
+    }
+    else {
+        Write-Host "Please choose a valid option 1-10"
+    }
+ }
