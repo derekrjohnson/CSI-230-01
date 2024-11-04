da9c02edae0adbc25f82108d06d98fac3bbee206
derekrj28 <derekrj28@gmail.com>
2024-11-04 12:07:42 -0500

    adgfafdad

--- week 11/lecture.bash
+++ week 11/lecture.bash
@@ -4,4 +4,10 @@ function pageCount () {
 	awk '{print $7}' /var/log/apache2/access.log | sort | uniq -c | sort -nr 
 }
 
-pageCount
+# pageCount
+
+function countingCurlAccess () {
+	grep 'curl/' /var/log/apache2/access.log | awk '{print $1}' | sort | uniq -c | sort -nr 	
+}
+
+countingCurlAccess
